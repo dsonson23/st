8a107866c3a1a1f7c6739c411a041aeb7907078e
Peter DHaiti <dsonson23@hotmail.com>
2017-02-27 18:15:16 -0500

    Installed uglify, grunt watch and did the overlay nav

---
+++ src/js/sentient.js
@@ -0,0 +1,8 @@
+// Fullscreen Overlay Nav
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
